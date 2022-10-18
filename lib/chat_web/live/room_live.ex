9a0ba5d806d12cc73729deea9ee5d6def705bd04
Lucas Carioca <lucascarioca97@gmail.com>
2022-10-18 00:44:02 -0300

    feat: add username mneumonic and real time functionality

--- lib/chat_web/live/room_live.ex
+++ lib/chat_web/live/room_live.ex
@@ -4,12 +4,37 @@ defmodule ChatWeb.RoomLive do
 
   @impl true
   def mount(%{"id" => room_id}, _session, socket) do
-    {:ok, assign(socket, room_id: room_id)}
+    topic = "room:" <> room_id
+    username = MnemonicSlugs.generate_slug(2)
+    if connected?(socket), do: ChatWeb.Endpoint.subscribe(topic)
+
+    {:ok,
+     assign(socket,
+       room_id: room_id,
+       topic: topic,
+       username: username,
+       message: "",
+       messages: [
+         %{uuid: UUID.uuid4(), content: "#{username} joined the chat...", username: "system"}
+       ],
+       temporary_assigns: [messages: []]
+     )}
   end
 
   @impl true
   def handle_event("submit_message", %{"chat" => %{"message" => message}}, socket) do
-    Logger.info(message: message)
-    {:noreply, socket}
+    message = %{uuid: UUID.uuid4(), content: message, username: socket.assigns.username}
+    ChatWeb.Endpoint.broadcast(socket.assigns.topic, "new-message", message)
+    {:noreply, assign(socket, message: "")}
+  end
+
+  @impl true
+  def handle_event("form_update", %{"chat" => %{"message" => message}}, socket) do
+    {:noreply, assign(socket, message: message)}
+  end
+
+  @impl true
+  def handle_info(%{event: "new-message", payload: message}, socket) do
+    {:noreply, assign(socket, messages: [message])}
   end
 end
